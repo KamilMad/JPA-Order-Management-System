974b494f6b38b18c3e0934a3a566edfa01c6ec45
KamilMad <kamilul085@gmail.com>
2024-12-25 22:58:40 +0100

    Created CustomrDao and save, findById methods

---
+++ JPA_Order_Management_System/src/main/java/dao/CustomerDao.java
@@ -0,0 +1,41 @@
+package dao;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.NoResultException;
+import jakarta.persistence.TypedQuery;
+import model.Customer;
+
+import java.util.List;
+
+public class CustomerDao {
+    private final EntityManagerFactory emf;
+
+
+    public CustomerDao(EntityManagerFactory emf) {
+        this.emf = emf;
+    }
+
+    public void save(Customer customer) {
+        try(EntityManager em = emf.createEntityManager()){
+            em.getTransaction().begin();
+            em.persist(customer);
+            em.getTransaction().commit();
+        }
+    }
+
+    public Customer findById(long id) {
+        Customer customer;
+        try(EntityManager em = emf.createEntityManager()){
+
+            String jpql = "SELECT c FROM Customer c WHERE c.id = :id";
+            TypedQuery<Customer> q = em.createQuery(jpql, Customer.class);
+            q.setParameter("id", id);
+            return q.getSingleResult();
+        }catch (NoResultException e) {
+            System.out.println("Customer with id " + id + " not found");
+            return null;
+        }
+    }
+    
+}
