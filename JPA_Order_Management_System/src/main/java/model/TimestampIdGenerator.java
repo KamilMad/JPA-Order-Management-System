4e73aa398aeae633fd15b058ee22eb8d5fbc784b
KamilMad <kamilul085@gmail.com>
2024-12-25 12:26:04 +0100

    Improved id strategy generator

--- JPA_Order_Management_System/src/main/java/model/TimestampIdGenerator.java
+++ JPA_Order_Management_System/src/main/java/model/TimestampIdGenerator.java
@@ -3,11 +3,18 @@ package model;
 import org.hibernate.engine.spi.SharedSessionContractImplementor;
 import org.hibernate.id.IdentifierGenerator;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 public class TimestampIdGenerator implements IdentifierGenerator {
+
+    private final AtomicLong counter = new AtomicLong();
     @Override
     public Object generate(SharedSessionContractImplementor sharedSessionContractImplementor, Object o) {
 
-        return (long)System.currentTimeMillis();
+        long timestamp = System.currentTimeMillis();
+        long uniqueId  = counter.incrementAndGet();
+
+        return Long.parseLong(timestamp + String.format("%03d", uniqueId));
 
     }
 }
