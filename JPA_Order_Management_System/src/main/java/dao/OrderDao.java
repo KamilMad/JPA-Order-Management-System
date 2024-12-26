001d263682c9a1313ebb87f1d37cdc4382ef3c32
KamilMad <kamilul085@gmail.com>
2024-12-26 19:10:34 +0100

    Added findAllOrdersForCustomer in Order class

--- JPA_Order_Management_System/src/main/java/dao/OrderDao.java
+++ JPA_Order_Management_System/src/main/java/dao/OrderDao.java
@@ -1,10 +1,33 @@
 package dao;
 
+import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.TypedQuery;
+import model.Customer;
 import model.Order;
+
+import java.util.List;
 
 public class OrderDao extends CrudDao<Order>{
 
+    private final EntityManagerFactory emf;
     public OrderDao(EntityManagerFactory emf) {
-        super(Order.class, emf);    }
+        super(Order.class, emf);
+        this.emf = emf;
+    }
+
+    public List<Order> findAllOrdersForCustomer(Customer customer){
+        try(EntityManager em = emf.createEntityManager()){
+            String jpql = """
+                    SELECT o
+                    FROM Order o
+                    WHERE o.customer.id = :customerId
+                    """;
+
+            TypedQuery<Order> q = em.createQuery(jpql, Order.class);
+            q.setParameter("customerId", customer.getId());
+            
+            return q.getResultList();
+        }
+    }
 }
