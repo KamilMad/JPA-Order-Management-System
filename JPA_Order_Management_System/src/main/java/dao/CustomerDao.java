34b1004b6d7b428ba5f0c7cd0d50f78385aa7176
KamilMad <kamilul085@gmail.com>
2024-12-26 18:48:38 +0100

    Added updateName and updateEmail methods to Customer class

--- JPA_Order_Management_System/src/main/java/dao/CustomerDao.java
+++ JPA_Order_Management_System/src/main/java/dao/CustomerDao.java
@@ -1,5 +1,6 @@
 package dao;
 
+import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import model.Customer;
 
@@ -12,5 +13,22 @@ public class CustomerDao extends CrudDao<Customer>{
         this.emf = emf;
     }
 
+    public void updateName(long id, String name){
+        try(EntityManager em = emf.createEntityManager()){
+            em.getTransaction().begin();
+            Customer customer = em.find(Customer.class, id);
+            customer.setName(name);
+            em.getTransaction().commit();
+        }
+    }
+
+    public void updateEmail(long id, String email){
+        try(EntityManager em = emf.createEntityManager()){
+            em.getTransaction().begin();
+            Customer customer = em.find(Customer.class, id);
+            customer.setName(email);
+            em.getTransaction().commit();
+        }
+    }
 
 }
